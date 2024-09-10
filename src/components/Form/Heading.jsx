392bac42e026a71a72b0fb1ce9eeace3d333e8e5
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-09-10 14:18:27 +0530

    Add ApplicationFee component and Heading component

---
+++ src/components/Form/Heading.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Heading = ({label, headerClass, textClass, spanClass}) => {
+  return (
+    <>
+    <div className={` dark:text-text-dark text-text-light px-2 ${headerClass}`}>
+        <h1 className={`text-xl font-thin py-2 ${textClass}`}>{label}</h1>
+    </div>
+        <span className={`block border-b-[.1px] border-gray-600 ${spanClass}`}></span>
+    </>
+  )
+}
+
+export default Heading;
