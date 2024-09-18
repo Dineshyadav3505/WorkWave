bc6076783ba5474c6573c619136f5424440cf0b4
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-09-18 14:25:27 +0530

    Add location and share functionality to Card component

--- src/app/(user)/admission/page.jsx
+++ src/app/(user)/admission/page.jsx
@@ -1,6 +1,9 @@
+"use client";
 import React from 'react'
 
 const page = () => {
+  const location = window.location.href;
+  console.log(location);
   return (
     <div>page</div>
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,51 +1,94 @@
 "use client";
+import React, { useState } from "react";
+import { shortenText } from "@/utils/StringShort";
+import Image from "next/image";
+import daysLeft from "@/utils/DaysLeft";
+import isNewUpdate, { removeSpace } from "@/utils/NewUpdate";
+import Share from "@/utils/Share";
 import Link from "next/link";
-import React from "react";
 
-const Card = () => {
+const Card = ({ data }) => {
+  const leftDays = daysLeft(data.lastDate);
+  const newUpdateStatus = isNewUpdate(data.updatedAt);
+  const link = removeSpace(data.postName);
+  const location = window.location.href;
+  const id = data._id;
+  const [copied, setCopied] = useState(false);
+
+  const share = async () => {
+    Share({ link, location, id });
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  };
+
   return (
-    <div className="w- full overflow-hidden h-52 relative">
-
-      {/* New */}
-      <div className="h-6 w-6 absolute top-0 right-0 z-10 flex justify-center items-center">
-        <h1 className="text-[10px] rotate-45 bg-red-500 leading-none px-10">
-          new
-        </h1>
-      </div>
+    <div className="w-full overflow-hidden h-52 relative">
+      {/* New Update Indicator */}
+      {newUpdateStatus && (
+        <div className="h-6 w-6 absolute top-0 right-0 z-10 flex justify-center items-center">
+          <h1 className="text-[10px] rotate-45 bg-red-500 leading-none px-10">
+            New
+          </h1>
+        </div>
+      )}
 
       {/* Image, PostName and Share button */}
-      <div className=" rounded-lg dark:bg-black bg-white p-2 shadow-lg flex flex-col relative ">
-        <div className=" border-b-[1px] pb-3 flex items-center p-2 gap-4 ">
-          <img
-            className="h-14 w-14 rounded-full"
-            src="https://images.unsplash.com/photo-1719937206498-b31844530a96?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt=""
-          />
+      <div className="rounded-lg dark:bg-black bg-white p-2 shadow-lg flex flex-col relative">
+        <div className="border-b-[1px] pb-3 flex items-center p-2 gap-4 h-20 bg-slate-200">
+          <div className="h-full w-14 overflow-hidden relative">
+            <Image
+              src={data.image}
+              alt="post"
+              width={100}
+              height={100}
+              className="rounded-full"
+            />
+          </div>
           <h1 className="text-sm w-full flex flex-wrap font-bold h-10 dark:text-white text-[#023E8A] overflow-hidden">
-            John Doe jaf iqsfd hsdfbsakhdchsafjkas dsfg dfgh xfghxcv dfhg dtyhnf
-            dtyhfgnb rtdhtyn{" "}
+            {shortenText(data.postName, 10)}
           </h1>
-          <button className="text-[#64C8FA]">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-5"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z"
-              />
-            </svg>
+
+          {/* share Button */}
+          <button onClick={share} className="text-[#64C8FA]">
+            {copied === true ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5A3.375 3.375 0 0 0 6.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0 0 15 2.25h-1.5a2.251 2.251 0 0 0-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 0 0-9-9Z"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z"
+                />
+              </svg>
+            )}
           </button>
         </div>
 
         {/* Begin Date and Last Date */}
-        <div className=" my-3 py-1 dark:bg-white bg-[#F4F4F4] rounded-full flex justify-around items-center px-1 text-text-light capitalize font-bold text-sm">
-          <h6 className=" ">Begin : 10/09/2024</h6>
+        <div className="my-3 py-1 dark:bg-[#bcbbbb] bg-[#F4F4F4] rounded-full flex justify-around items-center px-1 text-text-light capitalize font-bold text-sm">
+          <h6>Begin: {new Date(data.beginDate).toISOString().split("T")[0]}</h6>
           <span>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -58,23 +101,27 @@ const Card = () => {
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5"
+                d="m5.25 4.5l7.5 7.5-7.5 7.5m6-15l7.5 7.5-7.5 7.5"
               />
             </svg>
           </span>
           <h6>
-            Last date : <span className="text-green-700"> 10/09/2024 </span>{" "}
+            Last date:{" "}
+            <span className="text-green-700">
+              {new Date(data.lastDate).toISOString().split("T")[0]}
+            </span>
           </h6>
         </div>
 
-        {/* days Left  */}
-        <p className="text-red-600 text-sm mx-auto mb-4">13 days left</p>
-
+        {/* Days Left */}
+        <p className="text-red-600 text-sm mx-auto mb-4 capitalize">
+          {leftDays}
+        </p>
 
         {/* Link */}
-        <div className=" absolute -bottom-3  w-full flex justify-center ">
+        <div className="absolute -bottom-3 w-full flex justify-center">
           <Link
-            href="/user/1"
+            href={`/${link}`}
             className="flex gap-1 bg-[#023E8A] text-white px-5 rounded-full text-base justify-center items-center leading-7"
           >
             <span>
@@ -89,7 +136,7 @@ const Card = () => {
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z"
+                  d="M9 12.75L11.25 15l3-5.25m-3-7A11.959,11.959,0,0,1,3.598,6A11.99,11.99,0,0,0,3,9c0,5,.824,9,9,11C17,.332,21,.03,21,.03c-.21-.21-.21-.21-.21-.21s-.21-.21-.21-.21C15,.03,12,.03,9,.03c-.21-.21-.21-.21-.21-.21S12,.03,12,.03c3,.03,6,.03,9,.03c6,.03,9,.03,9,.03c-.21-.21-.21-.21-.21-.21s-.21-.21-.21-.21C15,.03,12,.03,9,.03c6,.03,9,.03c6,.03c6,.03c6,.03c6,.03c6,.03c6,.03c6,.03c6,.03c6,.03c6,.03c6,.03"
                 />
               </svg>
             </span>
