8b8db5823637ef83cafde2bdab42845a8ce797a2
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-09-18 19:15:29 +0530

    Refactor user admission page and user page components

--- src/app/(user)/page.jsx
+++ src/app/(user)/page.jsx
@@ -3,13 +3,12 @@ import Card from "@/components/Card";
 import SearchBar from "@/components/SearchBar";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Loader from "@/components/Loader";
 
 const Page = () => {
   const [searchResult, setSearchResult] = useState("");
   const [data, setData] = useState([]);
-  const [filteredData, setFilteredData] = useState([]);
   const [loading, setLoading] = useState(true);
-
 
   const handleSearch = (input) => {
     setSearchResult(input);
@@ -18,12 +17,15 @@ const Page = () => {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await axios.get('/api/post', {
+        const response = await axios.get("/api/post", {
           params: {
-            searchResult
-          }
+            searchResult,
+            page: 1,
+            limit: 12,
+            link : "applyLink",
+          },
         });
-        setData(response.data);
+        setData(response.data.posts);
         setLoading(false);
       } catch (error) {
         console.error(error);
@@ -32,18 +34,21 @@ const Page = () => {
     fetchProducts();
   }, [searchResult]);
 
-
   return (
     <div className="min-h-screen p-4">
       <div>
         <SearchBar placeholder="Search for job" onSearch={handleSearch} />
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-1 md:gap-5">
-        {data?.map((job) => (
-          <Card key={job._id} data={job} />
-        ))}
-      </div>
+      {loading === true ? (
+        <Loader/>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-1 md:gap-5">
+          {data?.map((job) => (
+            <Card key={job._id} data={job} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
