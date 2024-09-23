829e1c312ab7d7ac0d3efded9234fcb4d897a602
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-09-23 12:57:21 +0530

    Add post details page and API route

--- src/app/(user)/[postdetails]/page.jsx
+++ src/app/(user)/[postdetails]/page.jsx
@@ -1,19 +1,59 @@
-import React from 'react'
-import Image from 'next/image' 
+"use client";
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import axios from "axios";
+import ImageComponent from "@/components/postDetails/ImageComponent";
 
 const Post = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          "/api/post/details?id=66efd88a1324f42b81c37d63"
+        );
+        console.log(res.data.data); 
+        setData(res.data.data); 
+      } catch (err) {
+        console.error(err); 
+        setError(err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>; // Display error if it exists
+  }
+
+  if (!data) {
+    return <div>Loading...</div>; // Display loading state while fetching data
+  }
+
+
+
   return (
-    <div className=" p-4 min-h-screen relative">
+    <div className="min-h-screen relative">
+      <div className="w-full h-96 bg-red-100 relative">
         <Image
-          src="https://res.cloudinary.com/kodingmonk/image/upload/v1726928078/NaukriVacancy/s8kafnj1f8stxdzcjtth.png"      
+          src="https://res.cloudinary.com/kodingmonk/image/upload/v1726928078/NaukriVacancy/s8kafnj1f8stxdzcjtth.png"
           alt="hero"
-          layout="fill"
-          objectFit="cover"
-          quality={100}
-          priority={true}
+          fill
+          priority // Add this if the image is above the fold
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" // Adjust sizes based on your layout
+          style={{ objectFit: "cover" }} // Optional: to cover the area without distortion
         />
+      </div>
+      <ImageComponent data={data} />
+
+
+
+ 
     </div>
-  )
-}
+  );
+};
 
 export default Post;
---
+++ src/app/api/post/details/route.js
@@ -0,0 +1,34 @@
+import JobPost from "@/model/JobPost.Model";
+import { dbConnect } from "@/lib/dbConfig";
+import { NextResponse } from "next/server";
+
+export async function GET(req) {
+    await dbConnect(); // Ensure the database is connected
+    const query = req.url;
+    const url = new URL(query, `http://${req.headers.host}`);
+    
+    try {
+
+        const id = url.searchParams.get("id");
+        console.log("ID", id); 
+
+    const singlePost = await JobPost.findById(id);
+
+        if (!singlePost) {
+            return NextResponse.json(
+                { message: "Job post not found" },
+                { status: 404 }
+            );
+        }
+      return NextResponse.json({
+        message: "Job post found",
+        data: singlePost,
+      });
+    } catch (error) {
+      console.error("Error while fetching job posts", error);
+      return NextResponse.json(
+        { message: "Error while fetching job posts" },
+        { status: 500 }
+      );
+    }
+  }
