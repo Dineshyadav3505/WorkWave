4dc1cb41bfde29b94cb3e04ce7a599e57d2c08b7
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-09-27 22:27:39 +0530

    Add devDependencies and update layout.js and next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,33 @@
 /** @type {import('next').NextConfig} */
 import TerserPlugin from 'terser-webpack-plugin';
+// import withBundleAnalyzer from '@next/bundle-analyzer';
 
 const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"], // Add any other domains you need here
   },
   webpack: (config) => {
-    config.optimization.minimizer = [
-      new TerserPlugin({
-        terserOptions: {
-          compress: {
-            drop_console: true, // Remove console logs
+    // Ensure that we are modifying the existing minimizer
+    if (config.optimization && config.optimization.minimizer) {
+      config.optimization.minimizer.push(
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: true, // Remove console logs
+            },
+            output: {
+              comments: false, // Remove comments
+            },
           },
-          output: {
-            comments: false, // Remove comments
-          },
-        },
-      }),
-    ];
+        })
+      );
+    }
 
     return config;
   },
 };
 
-export default nextConfig;
+// // Export the configuration with bundle analyzer
+// export default withBundleAnalyzer({
+//   enabled: process.env.ANALYZE === 'true',
+// })(nextConfig);
